77269505ef41ec6b956c06437cca793b2aae633a
jamongjelly <88yangkh@gmail.com>
2021-09-16 23:52:05 +0900

    [백준] DFS / BFS

--- Java/BOJ/bs/silver/lv4/BOJ1920.java
+++ Java/BOJ/bs/silver/lv4/BOJ1920.java
@@ -1,4 +1,4 @@
-package temp.silver.lv4;
+package bs.silver.lv4;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+// 수 찾기
 public class BOJ1920 {
 
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
