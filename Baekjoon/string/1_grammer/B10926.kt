601ab07e52bd8471d8079bb4a242819455d352ae
m-dzn <88yangkh@gmail.com>
2022-06-17 21:15:08 +0900

    [Kotlin] 백준 10926번 ??!

---
+++ Baekjoon/string/1_grammer/B10926.kt
@@ -0,0 +1,11 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+// 백준 10926번 ??!
+fun main() {
+  val reader = BufferedReader(InputStreamReader(System.`in`))
+
+  println("${reader.readLine()}??!")
+
+  reader.close()
+}
