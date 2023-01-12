4a33dff0983b6893a3770fe5f96918573b4c2588
m-dzn <mdzn.dev@gmail.com>
2023-01-12 21:06:58 +0900

    📝 백준 : 단어 공부 풀이

---
+++ python/Baekjoon/sort/f/B1157.py
@@ -0,0 +1,19 @@
+# 브론즈 I > 단어 공부
+countArray = [0 for i in range(26)]
+
+word = input()
+maxIndex = 0
+
+for ch in word:
+    index = ord(ch.lower()) - ord('a')
+    countArray[index] += 1
+
+    if countArray[maxIndex] < countArray[index]:
+        maxIndex = index
+
+mostFrequentChar = chr(ord('a') + maxIndex).upper()
+numOfMaxChars = countArray.count(countArray[maxIndex])
+
+answer = mostFrequentChar if numOfMaxChars == 1 else '?'
+
+print(answer)
