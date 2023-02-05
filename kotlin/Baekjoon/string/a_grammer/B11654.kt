6ce478dd88170ea15d2c87abb9a71ac2f5df13a9
m-dzn <mdzn.dev@gmail.com>
2023-02-05 09:01:57 +0900

    :truck: Kotlin 백준 string 하위 디렉토리명 변경

--- kotlin/Baekjoon/string/a_grammer/B11654.kt
+++ kotlin/Baekjoon/string/a_grammer/B11654.kt
@@ -2,5 +2,5 @@ import java.io.*
 
 // 백준 11654번 아스키 코드
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-  readLine().forEach { println(it.code) }
+  readln().forEach { println(it.code) }
 }
