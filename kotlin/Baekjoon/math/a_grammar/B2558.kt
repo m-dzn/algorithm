fe0b95937545adec235a6fc66a435011fbdec3bb
m-dzn <mdzn.dev@gmail.com>
2023-02-05 09:25:22 +0900

    :memo: 백준 : A + B - 2 (Kotlin)

---
+++ kotlin/Baekjoon/math/a_grammar/B2558.kt
@@ -0,0 +1,9 @@
+package Baekjoon.math.a_grammar
+
+// 브론즈 V > A + B - 2
+fun main() = with(System.`in`.bufferedReader()) {
+    val A = readln().toInt()
+    val B = readln().toInt()
+    
+    println(A + B)
+}
