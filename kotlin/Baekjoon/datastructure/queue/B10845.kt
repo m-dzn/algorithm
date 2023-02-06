4e7d6b18971c05ca8c135e900b06d35f4df984e4
m-dzn <mdzn.dev@gmail.com>
2023-02-07 06:34:26 +0900

    :memo: 백준 : 큐 (Kotlin)

---
+++ kotlin/Baekjoon/datastructure/queue/B10845.kt
@@ -0,0 +1,52 @@
+package Baekjoon.datastructure.queue
+
+import java.util.*
+
+// 실버 IV > 큐
+fun main() {
+    val N = readln().toInt()
+    
+    repeat(N) {
+        val command = readln().split(' ')
+        
+        B10845.execute(command)
+    }
+    
+    B10845.print()
+}
+
+private object B10845 {
+    val sb = StringBuilder()
+    val queue: Queue<Int> = LinkedList()
+    
+    fun execute(command: List<String>) {
+        when (command[0]) {
+            "push" ->   queue.offer(command[1].toInt())
+            "pop" ->    sb.appendLine(pop())
+            "size" ->   sb.appendLine(queue.size)
+            "empty" ->  sb.appendLine(isEmpty())
+            "front" ->  sb.appendLine(front())
+            "back" ->   sb.appendLine(back())
+        }
+    }
+    
+    fun pop(): Int {
+        return if (queue.isNotEmpty()) queue.poll() else -1
+    }
+    
+    fun isEmpty(): Int {
+        return if (queue.isEmpty()) 1 else 0
+    }
+    
+    fun front(): Int {
+        return if (queue.isNotEmpty()) queue.peek() else -1
+    }
+    
+    fun back(): Int {
+        return if (queue.isNotEmpty()) queue.last() else -1
+    }
+    
+    fun print() {
+        println(sb)
+    }
+}
