17d6911b16fae4c3bb051d31875ca74416f1bc07
m-dzn <mdzn.dev@gmail.com>
2023-03-23 06:25:45 +0900

    :memo: 백준 : 마이크로소프트 로고 (Kotlin)

---
+++ kotlin/Baekjoon/string/a_grammer/B5338.kt
@@ -0,0 +1,13 @@
+package Baekjoon.string.a_grammer
+
+// 브론즈 V > 마이크로소프트 로고
+fun main() {
+    val logo = """       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|
+    """
+    
+    println(logo)
+}
