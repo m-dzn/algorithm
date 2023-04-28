e28f354be904a058c10f4a7df4de6178809e863a
m-dzn <mdzn.dev@gmail.com>
2023-04-28 09:03:01 +0900

    :memo: 백준 : 콜센터 (Kotlin)

---
+++ kotlin/Baekjoon/string/a_grammer/B5339.kt
@@ -0,0 +1,17 @@
+package Baekjoon.string.a_grammer
+
+// 브론즈 V > 콜센터
+fun main() {
+    val answer = """     /~\
+    ( oo|
+    _\=/_
+   /  _  \
+  //|/.\|\\
+ ||  \ /  ||
+============
+|          |
+|          |
+|          |"""
+    
+    println(answer)
+}
