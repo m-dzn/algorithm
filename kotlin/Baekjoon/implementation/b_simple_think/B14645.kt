736eb4fa3e15f3a3dc24526284883398fbfd39bb
m-dzn <mdzn.dev@gmail.com>
2023-05-10 18:38:26 +0900

    :memo: (Kotlin) 백준 : 와이버스 부릉부릉

---
+++ kotlin/Baekjoon/implementation/b_simple_think/B14645.kt
@@ -0,0 +1,10 @@
+package Baekjoon.implementation.b_simple_think
+
+// 브론즈 V > 와이버스 부릉부릉
+fun main() {
+    val (N, K) = readln().split(' ').map { it.toInt() }
+    
+    repeat(N) { readln() }
+    
+    println("비와이")
+}
