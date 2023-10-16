0f27f2f8f804e7dda94f428628cb4fde283c6f98
m-dzn <mdzn.dev@gmail.com>
2023-10-17 05:31:35 +0900

    :memo: (C#) 백준 : 오늘 날짜

---
+++ c#/Baekjoon/#grammar/print/B10699.cs
@@ -0,0 +1,14 @@
+using System;
+
+// 브론즈 V : 오늘 날짜
+namespace Baekjoon
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string today = DateTime.Today.ToString("yyyy-MM-dd");
+            Console.WriteLine(today);
+        }
+    }
+}
