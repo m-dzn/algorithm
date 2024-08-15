9c36178788061df80fb874b41de55ea8c3fb6581
m-dzn <mdzn.dev@gmail.com>
2024-08-15 16:55:45 +0900

    (C#) 백준 : 직사각형 https://www.acmicpc.net/problem/27323

---
+++ c#/Baekjoon/basic/geometry/B27323.cs
@@ -0,0 +1,13 @@
+﻿namespace geometry;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(A * B);
+    }
+}
