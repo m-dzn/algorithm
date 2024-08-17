797cecb6c27e416c4cad17d54b02d12e039432a1
Kyeongho Yang <mdzn.dev@gmail.com>
2024-08-17 13:00:36 +0900

    [Bronze V] Title: 개, Time: 44 ms, Memory: 5312 KB -BaekjoonHub

---
+++ C#/백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("|\\_/|");
+        Console.WriteLine("|q p|   /}");
+        Console.WriteLine("( 0 )\"\"\"\\");
+        Console.WriteLine("|\"^\"`    |");
+        Console.WriteLine("||_/=\\\\__|");
+    }
+}
