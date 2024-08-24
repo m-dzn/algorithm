95c0a1ff8342803995ff1f11fddcd2b4d1e2cb5f
Kyeongho Yang <mdzn.dev@gmail.com>
2024-08-25 07:49:55 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,18 @@
+N, K = list(map(int, input().split()))
+
+coins = []
+
+for i in range(N):
+    coin = int(input())
+    coins.append(coin)
+
+remainder = K
+num_of_coins = 0
+for coin in reversed(coins):
+    num_of_coins += remainder // coin
+    remainder %= coin
+
+    if remainder == 0:
+        break
+
+print(num_of_coins)
