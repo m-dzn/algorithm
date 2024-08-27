c75e317c765922d97af7da1f841f69db600822a0
Kyeongho Yang <mdzn.dev@gmail.com>
2024-08-27 23:03:09 +0900

    [Silver V] Title: 다리 놓기, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,13 @@
+def factorial(n):
+    num = 1
+    for i in range(1, n + 1):
+        num *= i
+    return num
+
+# 입력
+T = int(input())
+
+for _ in range(T):
+    N, M = map(int, input().split())
+    bridge = factorial(M) // (factorial(N) * factorial(M - N))
+    print(bridge)
