0d3200c0c8ef6f4ed8eda0ea1b6c93b8fba6961a
Kyeongho Yang <mdzn.dev@gmail.com>
2024-08-29 15:19:02 +0900

    [Silver V] Title: 거스름돈, Time: 88 ms, Memory: 12336 KB -BaekjoonHub

---
+++ Kotlin (JVM)/백준/Silver/14916. 거스름돈/거스름돈.kt
@@ -0,0 +1,32 @@
+// 실버 V > 거스름 돈
+fun main() {
+    val n = readln().toInt()
+    
+    val numOfCoins = getNumOfCoins(n)
+    println(numOfCoins)
+}
+
+private fun getNumOfCoins(n: Int): Int {
+    var remain = n
+    var numOfCoins = 0
+    
+    while (remain > 0) {
+        if (remain % 5 == 0) {
+            numOfCoins += remain / 5
+            break
+        } else if (remain % 2 == 0) {
+            val coin2 = (remain % 10) / 2
+            val coin5 = remain / 10 * 2
+            
+            numOfCoins += coin2 + coin5
+            break
+        } else {
+            remain -= 5
+            numOfCoins++
+        }
+    }
+    
+    if (remain < 0) return -1
+    
+    return numOfCoins
+}
