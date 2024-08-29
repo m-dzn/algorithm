ea31a6f95996a10a7a09172628ae32f70ee733b0
Kyeongho Yang <mdzn.dev@gmail.com>
2024-08-29 21:55:18 +0900

    [Bronze IV] Title: 세수정렬, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Bronze/2752. 세수정렬/세수정렬.py
+++ Python/백준/Bronze/2752. 세수정렬/세수정렬.py
@@ -1,39 +1,19 @@
-def merge_sort(array, left, right):
-  if left < right:
-    mid = left + (right - left) // 2
-    merge_sort(array, left, mid)
-    merge_sort(array, mid + 1, right)
-    merge(array, left, mid, right)
+def sort(array):
+  for i in range(len(array)):
+    for j in range(i, len(array)):
+      if array[i] > array[j]:
+        swap(array, i, j)
 
-def merge(array, left, mid, right):
-  temp_array = []
-
-  lp = left
-  rp = mid + 1
-
-  while lp <= mid and rp <= right:
-    if array[lp] <= array[rp]:
-      temp_array.append(array[lp])
-      lp += 1
-    else:
-      temp_array.append(array[rp])
-      rp += 1
-
-  while lp <= mid:
-    temp_array.append(array[lp])
-    lp += 1
-  while rp <= right:
-    temp_array.append(array[rp])
-    rp += 1
-
-  for i in range(left, right + 1):
-    array[i] = temp_array[i - left]
+def swap(array, i, j):
+  temp = array[i]
+  array[i] = array[j]
+  array[j] = temp
 
 # 입력
 numbers = list(map(int, input().split()))
 
 # 처리
-merge_sort(numbers, 0, len(numbers) - 1)
+sort(numbers)
 
 # 출력
 print(*numbers)
