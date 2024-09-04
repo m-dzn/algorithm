cd2411d3692441d78739997eb17792181beae170
Kyeongho Yang <mdzn.dev@gmail.com>
2024-09-04 21:27:27 +0900

    [Bronze V] Title: 꼬마 정민, Time: 116 ms, Memory: 14340 KB -BaekjoonHub

---
+++ Kotlin (JVM)/백준/Bronze/11382. 꼬마 정민/꼬마 정민.kt
@@ -0,0 +1,11 @@
+// 브론즈 V > 꼬마 정민
+fun main() = with(System.`in`.bufferedReader()) {
+    val sum = readLine().split(' ').sumOf { it.toLong() }
+    
+    val bw = System.out.bufferedWriter()
+    
+    bw.write(sum.toString())
+    
+    bw.flush()
+    bw.close()
+}
