4b9962f29a6d869397709cc7eba97ab746155f47
Kyeongho Yang <mdzn.dev@gmail.com>
2024-09-09 22:42:38 +0900

    [Silver IV] Title: 스택 2, Time: 780 ms, Memory: 102856 KB -BaekjoonHub

--- Python/백준/Silver/28278. 스택 2/스택 2.py
+++ Python/백준/Silver/28278. 스택 2/스택 2.py
@@ -8,21 +8,21 @@ N = int(input())
 
 # 처리
 for _ in range(N):
-    array = list(map(int, input().split()))
+    array = input().split()
 
     command = array[0]
-    if command == 1:
+    if command == '1':
         stack.append(array[1])
-    elif command == 2:
+    elif command == '2':
         if stack:
             print(stack.pop())
         else:
             print(-1)
-    elif command == 3:
+    elif command == '3':
         print(len(stack))
-    elif command == 4:
+    elif command == '4':
         print(1 if not stack else 0)
-    elif command == 5:
+    elif command == '5':
         if stack:
             print(stack[-1])
         else:
