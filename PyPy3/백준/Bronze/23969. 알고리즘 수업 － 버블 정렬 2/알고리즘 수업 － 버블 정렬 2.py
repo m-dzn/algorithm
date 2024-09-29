713d0cd0a101fd9cba4024038f41e08a454d0f40
Kyeongho Yang <mdzn.dev@gmail.com>
2024-09-30 08:05:54 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 2, Time: 324 ms, Memory: 110696 KB -BaekjoonHub

---
+++ PyPy3/백준/Bronze/23969. 알고리즘 수업 － 버블 정렬 2/알고리즘 수업 － 버블 정렬 2.py
@@ -0,0 +1,19 @@
+# 입력
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+
+# 처리
+count = 0
+
+for last in range(len(A) - 1, 0, -1):
+    for i in range(last):
+        if A[i] > A[i + 1]:
+            A[i], A[i + 1] = A[i + 1], A[i]
+            count += 1
+
+            if count == K:
+                print(*A)
+                exit()
+
+# 출력
+print(-1)
