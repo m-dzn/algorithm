0f1e40a319a47815a4866199dc252677fb53e42e
Kyeongho Yang <mdzn.dev@gmail.com>
2024-10-22 04:59:35 +0900

    [Silver IV] Title: 차집합, Time: 500 ms, Memory: 256172 KB -BaekjoonHub

--- PyPy3/백준/Silver/1822. 차집합/차집합.py
+++ PyPy3/백준/Silver/1822. 차집합/차집합.py
@@ -3,11 +3,9 @@ len_A, len_B = map(int, input().split())
 A = set(map(int, input().split()))
 B = set(map(int, input().split()))
 
-A_minus_B = A - B
+A_minus_B = A.difference(B)
 
 print(len(A_minus_B))
 
-if len(A_minus_B) > 0:
-    sorted_elements = list(A_minus_B)
-    sorted_elements.sort()
-    print(*sorted_elements)
+for num in sorted(A_minus_B):
+    print(num, end = ' ')
