0f3ed5e33905936e52d8f6c437336ad8b3c63343
Kyeongho Yang <mdzn.dev@gmail.com>
2025-01-02 15:15:19 +0900

    [Silver II] Title: 좌표 압축, Time: 1060 ms, Memory: 284200 KB -BaekjoonHub

---
+++ PyPy3/백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+# 데이터 전처리
+n = int(input())
+arr = list(map(int, input().split()))
+
+unique_arr = list(set(arr))
+sorted_arr = sorted(unique_arr)
+
+dict = { sorted_arr[i]: i for i in range(len(sorted_arr)) }
+
+for i in arr:
+    print(dict[i], end=' ')
