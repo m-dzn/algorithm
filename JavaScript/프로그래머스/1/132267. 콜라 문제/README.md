22cf5b9cda7dff75e1cb5a9a627ba7e77a6ba665
Kyeongho Yang <mdzn.dev@gmail.com>
2025-01-10 01:41:04 +0900

    [level 1] Title: 콜라 문제, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/132267. 콜라 문제/README.md
+++ JavaScript/프로그래머스/1/132267. 콜라 문제/README.md
@@ -1,10 +1,10 @@
 # [level 1] 콜라 문제 - 132267 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132267?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132267) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.26 ms
+메모리: 33.4 MB, 시간: 0.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 09일 22:03:45
+2025년 01월 10일 01:41:02
 
 ### 문제 설명
 
--- JavaScript/프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ JavaScript/프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,46 +1,44 @@
-// Input : 빈 병의 개수
+/* Input
+ * a: a개의 빈 병 당
+ * b: b개의 새 콜라 교환
+ * n: 처음 가져간 빈 병의 개수
+ */
+
 function solution(
     a,
     b,
     n
 ) {
+    let emptyBottleCount = n;
+    
+    let answer = 0;
+    
+    /* Process : 처리 */
+    
     // 1단계: 교환비를 구한다
     const exchangeRatio = b / a
     
-    // a개 미만의 병은 버린다
-    
-    let emptyBottleCount = n;
-    
-    
-    /* 데이터셋 1번 (1/2) */
-    // 바꿔먹을 빈병 -> 새로 받은 병    빈 병
-
-    // 1차: 20    ->     10
-    // 2차: 10    ->     5
-    // 3차: 5     ->     2         + 1
-    // 4차: 3     ->     1         + 1
-    // 5차: 2     ->     1
-    
     // 2단계: "a <= 바꿔먹을 빈병"(종료 조건) 이 될 때까지 반복한다
-    let answer = 0;
-    
-    while (a <= emptyBottleCount) {
-        const input = emptyBottleCount;
-        
-        // 새로 받은 병을 구한다
+    while (a <= emptyBottleCount) {        
+        // 2-1: 새로 받은 콜라 개수를 구한다
         const newCokes = Math.floor(emptyBottleCount / a) * b;
+        
+        // 2-2: 교환하지 못하고 남은 빈 병 개수를 구한다
         const remains = emptyBottleCount % a;
         
-        // 새로 받은 병을 더해준다
+        // 2-2: 새로 받은 콜라의 누적 개수를 기록한다
         answer += newCokes;
         
-        // 그리고 새로 받은 콜라를 다 마신다
+        // 2-3: 새로 받은 콜라를 다 마셔 빈 병으로 만든다
         emptyBottleCount = newCokes;
         
-        // 절사해버린 빈 병을 더해준다
+        // 2-4: 새 빈 병과 교환하지 못해 남은 빈 병의 개수를 더한다
         emptyBottleCount += remains;
     }
     
-    // Output : 받을 수 있는 콜라의 병 수
     return answer;
 }
+
+/* Output
+ * answer: 받을 수 있는 콜라의 병 수
+ */
